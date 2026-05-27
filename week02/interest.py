4b1b8ca1638d31584f82899913402bf32e1d0071
Leo Irakliotis <lgreco@gmail.com>
2026-05-27 12:22:37 -0500

    all

--- week02/interest.py
+++ week02/interest.py
@@ -4,8 +4,8 @@ term = int(input("Term in years: "))
 
 total = principal * (1+interest_rate) ** term
 
-print("A principal of ", principal, " at ", interest_rate, " after ", term, "years yields ", total)
-
-# Nice formatting
-
-print(f"A principal of ${principal:,d} at {100*interest_rate:.2f}% yields after {term} years, yields ${total:,.2f}")
+print("-----------------")
+print("Principal amount:", principal)
+print("   Interest rate:", interest_rate)
+print("Term of maturity:", term)
+print(" Compound amount:", total)
