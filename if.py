93f73b2b45ec6d3c9ee867d73dedc185a6f709df
Leo Irakliotis <lgreco@gmail.com>
2026-07-06 21:22:32 -0500

    twine

--- if.py
+++
@@ -1,9 +0,0 @@
-
-n = int(input("Give me a positive integer number. "))
-
-remainder_after_divide_by_2 = n - 2 * (n // 2)
-
-if remainder_after_divide_by_2 == 0:
-    print("The number is even")
-else:
-    print("The number is odd")
