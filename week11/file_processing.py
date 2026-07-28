129448b92df974007414fa1d8d75f313341a43db
Leo Irakliotis <lgreco@gmail.com>
2026-07-28 11:47:22 -0500

    8a55d68852c13e51d2827ec206f84918

--- week11/file_processing.py
+++ week11/file_processing.py
@@ -1,1 +1,12 @@
 # A naive example for fileprocessing
+
+contents = []  # unique words in a file
+frequency = []  # frequency[i] is the number of times contents[i] appears
+
+file_to_process = "tale_of_two_cities.txt"
+
+with open(file_to_process, 'r') as f:
+    line = f.readline()
+    while line is not None:  # line just read, not empty
+        words = line.split()  # A list with the words of the current line
+
