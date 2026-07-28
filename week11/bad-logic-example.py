c801e3fa1bf617841ebb5d8b493046b31692d154
Leo Irakliotis <lgreco@gmail.com>
2026-07-28 12:11:14 -0500

    c41fa25091939473578f86858f7a3a6b

---
+++ week11/bad-logic-example.py
@@ -0,0 +1,29 @@
+# A naive example for fileprocessing
+
+contents = []  # unique words in a file
+frequency = []  # frequency[i] is the number of times contents[i] appears
+
+file_to_process = "tale_of_two_cities.txt"
+
+with open(file_to_process, 'r') as f:
+    line = f.readline()
+    while line is not None:  # line just read, not empty
+        words = line.split()  # A list with the words of the current line
+        # Consider every word in this line:
+        for word in words:
+            is_unique = False
+            # Search through contents to see if word already there
+            i = 0
+            while i < len(contents) and is_unique:
+                is_unique = contents[i] == word
+                i += 1
+            # When this while loop ends, it ends either because we did
+            # not find `word` in `contents` or because we foud it
+            # at index position `i-1`
+            if is_unique:
+                contents.append(word)
+                frequency.append(1)
+            else:
+                frequency[i-1] = frequency[i-1] + 1  # same as frequency[i-1]+=1
+        line = f.readline()
+
